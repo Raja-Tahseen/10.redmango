a50ca2168acb365cfa70ec0c1012b5c557ea00df
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-21 20:24:38 +0500

    219 - Fetch Menu Items Interfaces in TS. (Using localstate - useState(), useEffect())

--- redmango/src/Container/App.tsx
+++ redmango/src/Container/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Header, Footer } from "../Components/Layout";
+import { useState, useEffect } from "react"; 
+import { menuItemModel } from "../Interfaces";
 
 
 function App() {
-
-  return (    
+ const [menuItems,setMenuItems] = useState<menuItemModel[]>([]);
+ useEffect(() => {
+  fetch("https://localhost:7181/api/MenuItem").then((response)=> response.json()).then((data)=>{
+    console.log(data);
+    setMenuItems(data.result);
+  });
+ },[]);
+  return (
       <div>
         <Header />
         Main Component
