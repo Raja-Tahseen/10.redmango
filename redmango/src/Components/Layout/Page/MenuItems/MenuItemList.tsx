dcdcb8ae26ddbdeed11504d9ac899a4b43b2fa88
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-21 20:44:59 +0500

    220 - Better Structuring App

---
+++ redmango/src/Components/Layout/Page/MenuItems/MenuItemList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { menuItemModel } from "../../../../Interfaces";
+
+function MenuItemList() {
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7181/api/MenuItem")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMenuItems(data.result);
+      });
+  }, []);
+
+  return <div>MenuItemList</div>;
+}
+
+export default MenuItemList;
--- redmango/src/Container/App.tsx
+++ redmango/src/Container/App.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { Header, Footer } from "../Components/Layout";
-import { useState, useEffect } from "react"; 
-import { menuItemModel } from "../Interfaces";
+import {Home} from "../Pages";
 
 
 function App() {
- const [menuItems,setMenuItems] = useState<menuItemModel[]>([]);
- useEffect(() => {
-  fetch("https://localhost:7181/api/MenuItem").then((response)=> response.json()).then((data)=>{
-    console.log(data);
-    setMenuItems(data.result);
-  });
- },[]);
+ 
   return (
       <div>
         <Header />
-        Main Component
+        <Home />
         <Footer />
       </div>
   )
