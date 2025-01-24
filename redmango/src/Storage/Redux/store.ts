14dcfb1f3dfb5708ebe747ea42d6941041ca1883
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-24 07:41:50 +0500

    226 - Add query to fetch menu items

--- redmango/src/Storage/Redux/store.ts
+++ redmango/src/Storage/Redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { menuItemReducer } from "./menuItemSlice";
+import { menuItemApi } from "../../Apis";
 
 const store = configureStore({
     reducer: {
         menuItemStore: menuItemReducer,
+        [menuItemApi.reducerPath]: menuItemApi.reducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(menuItemApi.middleware),
 });
 
 //working with TypeScript, we basically have to export the root state and that will be the type of store state.
