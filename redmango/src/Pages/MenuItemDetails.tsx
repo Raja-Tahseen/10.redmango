fd81aec1246fb71be0c2d8f1923187876ab5a73f
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-24 20:57:21 +0500

    227 - Call Slice to Fetch Data, 228 - Display Loading Text, 229 - Assignment 20 Load Menu Item Details, 230 - Assignment 20 Solution Load Menu Item Details

--- redmango/src/Pages/MenuItemDetails.tsx
+++ redmango/src/Pages/MenuItemDetails.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { menuItemModel } from "../Interfaces";
-
-interface Props {
-  menuItem: menuItemModel;
-}
+import { useNavigate, useParams } from "react-router-dom";
+import { useGetMenuItemByIdQuery } from  "../Apis/menuItemApi";
 
 function MenuItemDetails() {
+  const {menuItemId} = useParams();
+  const {data,isLoading} = useGetMenuItemByIdQuery(menuItemId);
+  const navigate = useNavigate();
+
   return (
     <div className="container pt-4 pt-md-5">
-      <div className="row">
+      {!isLoading? (
+        <div className="row">
         <div className="col-7">
-          <h2 className="text-success">NAME</h2>
+          <h2 className="text-success">{data.result?.name}</h2>
           <span>
             <span
               className="badge text-bg-dark pt-2"
               style={{ height: "40px", fontSize: "20px" }}
             >
-              CATEGORY
+              {data.result?.category}
             </span>
           </span>
           <span>
@@ -24,13 +27,14 @@ function MenuItemDetails() {
               className="badge text-bg-light pt-2"
               style={{ height: "40px", fontSize: "20px" }}
             >
-              SPECIAL TAG
+              {data.result?.specialTag}
+
             </span>
           </span>
           <p style={{ fontSize: "20px" }} className="pt-2">
-            DESCRIPTION
+          {data.result?.description}
           </p>
-          <span className="h3">$10</span> &nbsp;&nbsp;&nbsp;
+          <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;&nbsp;
           <span
             className="pb-2  p-3"
             style={{ border: "1px solid #333", borderRadius: "30px" }}
@@ -53,7 +57,7 @@ function MenuItemDetails() {
             </div>
 
             <div className="col-5 ">
-              <button className="btn btn-secondary form-control">
+              <button className="btn btn-secondary form-control" onClick={() => navigate(-1)}>
                 Back to Home
               </button>
             </div>
@@ -61,13 +65,17 @@ function MenuItemDetails() {
         </div>
         <div className="col-5">
           <img
-            src="https://placehold.co/150"
+            src={`https://localhost:7181/images/${data.result.image}`}
             width="100%"
             style={{ borderRadius: "50%" }}
             alt="No content"
           ></img>
         </div>
       </div>
+
+      ) : (<div className="d-flex justify-content-center" style={{ width: "100%"}}>
+      <div>Loading...</div></div>)}
+      
     </div>
   );
 }
