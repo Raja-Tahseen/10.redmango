d51f5978ed776d9d78f7a6b410328d3c43300dfc
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-26 17:09:32 +0500

    238 - Mini Loader Component (Create a common folder for it and reuse in app)

--- redmango/src/Components/Layout/Page/MenuItems/MenuItemCard.tsx
+++ redmango/src/Components/Layout/Page/MenuItems/MenuItemCard.tsx
@@ -3,6 +3,7 @@ import { menuItemModel } from "../../../../Interfaces";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useUpdateShoppingCartMutation } from "../../../../Apis/shoppingCartApi";
+import {MiniLoader} from "../Common";
 
 interface Props {
   menuItem: menuItemModel;
@@ -19,7 +20,6 @@ function MenuItemCard(props: Props) {
       updateQuantityBy:1,
       userId:'39cd62ee-2ef0-4fc3-9a75-e26bc0147aaf'
     });
-    console.log(response);
     setIsAddingToCart(false);
   }
 
@@ -64,11 +64,7 @@ function MenuItemCard(props: Props) {
               top: "15px",
               right: "15px"
             }}>
-              <div 
-              className={"spinner-border text-warning"}
-              style={{scale: "100%"}}>
-                {" "}
-              </div>
+              <MiniLoader />
             </div>
             ) : (
             <i
