0c5b589f44a22dd0a8089ed157123f1cbe9ac950
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-27 19:32:47 +0500

    245 - Add Reducers (updateQuantity, removeFromCart) to shoppingCartSlice

--- redmango/src/Storage/Redux/shoppingCartSlice.ts
+++ redmango/src/Storage/Redux/shoppingCartSlice.ts
@@ -1,7 +1,8 @@
 //-- shoppingCartSlice.ts manages menu item data in the global state.
 import { createSlice } from "@reduxjs/toolkit";
+import { shoppingCartModel } from "../../Interfaces";
 
-const initialState = {
+const initialState : shoppingCartModel = {
   cartItems: [], //An array to store fetched cart items.
 };
 
@@ -13,8 +14,26 @@ export const shoppingCartSlice = createSlice({
       //Updates the cartItems state with fetched data.
       state.cartItems = action.payload;
     },
+    updateQuantity: (state, action) => {
+      //payload will have cartitem that needs to be updated with new quantity
+      state.cartItems = state.cartItems?.map((item) => {
+        if (item.id === action.payload.cartitem.id) {
+          item. quantity = action.payload.quantity;
+        }
+        return item;
+      });
+    },
+    removeFromCart: (state, action) => {
+      //payload will have cartitem that needs to be removed from cart
+      state.cartItems = state.cartItems?.filter((item) => {
+        if (item.id === action.payload.cartitem.id) {
+          return null;
+        }
+        return item;
+      });
+    }
   },
 });
 
-export const { setShoppingCart } = shoppingCartSlice.actions;
+export const { setShoppingCart, updateQuantity, removeFromCart } = shoppingCartSlice.actions;
 export const shoppingCartReducer = shoppingCartSlice.reducer;
