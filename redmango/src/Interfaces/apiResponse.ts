6dc0caf83a6db2b087f3fdad408b0440a59c5954
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-31 08:26:06 +0500

    256 - Response API Interface (model)

---
+++ redmango/src/Interfaces/apiResponse.ts
@@ -0,0 +1,13 @@
+export default interface apiResponse {
+    data?: {
+        //this will be included in the suggestions by typescipt so if possible use the format if you know that
+        statusCode?: number;
+        isSuccess?: boolean;
+        errorMessages?: Array<string>;
+        result:{
+            // typescript will not give suggestions
+            [key:string] : string
+        };
+    };
+    error?: any;
+}
--- redmango/src/Interfaces/index.ts
+++ redmango/src/Interfaces/index.ts
@@ -2,5 +2,6 @@ import menuItemModel from "./menuItemModel";
 import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel.ts";
+import apiResponse from "./apiResponse.ts";
 
-export type { menuItemModel, cartItemModel, shoppingCartModel, userModel };
+export type { menuItemModel, cartItemModel, shoppingCartModel, userModel, apiResponse };
