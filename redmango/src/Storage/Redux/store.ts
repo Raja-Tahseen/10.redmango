2091067cd4d052bdc9a29aa3ea026dd760477414
Raja Tahseen <rajatahseen86@outlook.com>
2025-01-31 07:48:00 +0500

    254 - Authentiation Slice and User Interface

--- redmango/src/Storage/Redux/store.ts
+++ redmango/src/Storage/Redux/store.ts
@@ -1,18 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { menuItemReducer } from "./menuItemSlice";
 import { shoppingCartReducer } from "./shoppingCartSlice";
-import { menuItemApi, shoppingCartApi } from "../../Apis";
+import { userAuthReducer } from "./userAuthSlice";
+import { authApi, menuItemApi, shoppingCartApi } from "../../Apis";
 
 const store = configureStore({
     reducer: {
         menuItemStore: menuItemReducer,//Manages the local state of menu items.
         shoppingCartStore: shoppingCartReducer,
+        userAuthStore: userAuthReducer,
         [menuItemApi.reducerPath]: menuItemApi.reducer,//Handles API-related state using RTK Query.
         [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
+        [authApi.reducerPath]: authApi.reducer,        
     },
     middleware: (getDefaultMiddleware) => 
         getDefaultMiddleware()
     .concat(menuItemApi.middleware)
+    .concat(authApi.middleware)
     .concat(shoppingCartApi.middleware),//Default middleware is extended to include menuItemApi.middleware for handling API caching and invalidation.
 });
 
