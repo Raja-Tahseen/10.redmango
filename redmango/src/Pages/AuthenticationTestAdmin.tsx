a3534e7f8ed17acaf5321ce96e3bbea5bdbc733e
Raja Tahseen <rajatahseen86@outlook.com>
2025-02-02 04:48:25 +0500

    269 - Authorization with HOC

--- redmango/src/Pages/AuthenticationTestAdmin.tsx
+++ redmango/src/Pages/AuthenticationTestAdmin.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { withAdminAuth } from '../HOC';
 
 function AuthenticationTestAdmin() {
   return (
@@ -6,4 +6,4 @@ function AuthenticationTestAdmin() {
   )
 }
 
-export default AuthenticationTestAdmin
+export default withAdminAuth(AuthenticationTestAdmin);
