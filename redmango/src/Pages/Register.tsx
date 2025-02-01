601596857ee05c768791186f43ae5ae6a61d35e3
Raja Tahseen <rajatahseen86@outlook.com>
2025-02-01 20:49:07 +0500

    266 - Add Loader on Register

--- redmango/src/Pages/Register.tsx
+++ redmango/src/Pages/Register.tsx
@@ -4,6 +4,7 @@ import { inputHelper, toastNotify } from '../Helper';
 import { useRegisterUserMutation } from '../Apis/authApi';
 import { apiResponse } from '../Interfaces';
 import { useNavigate } from 'react-router-dom';
+import { MainLoader } from '../Components/Page/Common';
 
 
 function Register() {
@@ -45,6 +46,7 @@ function Register() {
 
   return (
     <div className="container text-center">
+      {loading && <MainLoader />}
     <form method="post" onSubmit={handleSubmit}>
       <h1 className="mt-5">Register</h1>
       <div className="mt-5">
@@ -95,7 +97,7 @@ function Register() {
         </div>
       </div>
       <div className="mt-5">
-        <button type="submit" className="btn btn-success">
+        <button type="submit" className="btn btn-success" disabled={loading}>
           Register
         </button>
       </div>
