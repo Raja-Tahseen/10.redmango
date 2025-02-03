0554ef3945429628c62c77897d93f1cb90873aa8
Raja Tahseen <rajatahseen86@outlook.com>
2025-02-03 16:27:40 +0500

    278 - Stripe Card Demo

--- redmango/src/Pages/Payment.tsx
+++ redmango/src/Pages/Payment.tsx
@@ -1,14 +1,31 @@
 import { useLocation } from "react-router-dom";
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import { PaymentForm } from "../Components/Page/Payment";
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
 
-  console.log(apiResult);
-  console.log(userInput);
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+// Stripe Publish Key
+const stripePromise = loadStripe('pk_test_51QoKzSEJYDXr859PZTnbKIeAcnmr6wCOUn4v9A7a5HlrHqXSgd9NyjcMT7ie3Ylb5uPvFZVAXjC2IrvdeYvYh0My005i3hT3nS');
 
-  return <div>Payment</div>;
+const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
+
+//   console.log(apiResult);
+//   console.log(userInput);
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
