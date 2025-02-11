b4fa292cdd9dff8b62142176dd3ce63302a637bf
Raja Tahseen <rajatahseen86@outlook.com>
2025-02-11 19:43:57 +0500

    304 - Assignment 23 Dynamic Menu Item List

--- redmango/src/Pages/MenuItem/MenuItemList.tsx
+++ redmango/src/Pages/MenuItem/MenuItemList.tsx
@@ -1,46 +1,79 @@
-import React from "react";
+import { useGetMenuItemsQuery } from "../../Apis/menuItemApi";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { MainLoader } from "../../Components/Page/Common";
+import { setMenuItem } from "../../Storage/Redux/menuItemSlice";
+import { menuItemModel } from "../../Interfaces";
 
 function MenuItemList() {
+  const { data, isLoading } = useGetMenuItemsQuery(null);
+  const dispatch = useDispatch();
+
+  //   useEffect(() => {
+  //     if (!isLoading) {
+  //       dispatch(setMenuItem(data.result));
+  //       console.log(data.result);
+  //     }
+  //   }, [isLoading]);
+//   if (isLoading) {
+//     return (
+//       <div>
+//         <MainLoader />
+//       </div>
+//     );
+//   }
+
   return (
-    <div className="table p-5">
-      <div className="d-flex align-items-center justify-content-between">
-        <h1 className="text-success">MenuItem List</h1>
-        <button className="btn btn-success">Add New</button>
-      </div>
-      <div className="p-2">
-        <div className="row border">
-          <div className="col-2">Image</div>
-          <div className="col-1">ID</div>
-          <div className="col-2">Name</div>
-          <div className="col-2">Category</div>
-          <div className="col-1">Price</div>
-          <div className="col-2">Special Tag</div>
-          <div className="col-1">Action</div>
+    <>
+    {isLoading && <MainLoader />}
+    {!isLoading && (
+        <div className="table p-5">
+        <div className="d-flex align-items-center justify-content-between">
+          <h1 className="text-success">MenuItem List</h1>
+          <button className="btn btn-success">Add New</button>
         </div>
-        <div className="row border">
-          <div className="col-2">
-            <img
-              src="https://via.placeholder.com/150"
-              alt="no content"
-              style={{ width: "100%", maxWidth: "120px" }}
-            />
+        <div className="p-2">
+          <div className="row border">
+            <div className="col-1">Image</div>
+            <div className="col-1">ID</div>
+            <div className="col-3">Name</div>
+            <div className="col-2">Category</div>
+            <div className="col-1">Price</div>
+            <div className="col-2">Special Tag</div>
+            <div className="col-1">Action</div>
           </div>
-          <div className="col-1">.id</div>
-          <div className="col-2">.name</div>
-          <div className="col-2">.category</div>
-          <div className="col-1">.price</div>
-          <div className="col-2">.specialTag</div>
-          <div className="col-1">
-            <button className="btn btn-success">
-              <i className="bi bi-pencil-fill"></i>
-            </button>
-            <button className="btn btn-danger mx-2">
-              <i className="bi bi-trash-fill"></i>
-            </button>
-          </div>
+          {data.result.length > 0 &&
+            data.result.map((menuItem: menuItemModel, index: number) => {
+              return (
+                <div className="row border" key={menuItem.id}>
+                  <div className="col-1">
+                    <img
+                      src={`https://localhost:7181/images/${menuItem.image}`}
+                      alt={menuItem.name}
+                      style={{ width: "100%", maxWidth: "120px" }}
+                    />
+                  </div>
+                  <div className="col-1">{menuItem.id}</div>
+                  <div className="col-3">{menuItem.name}</div>
+                  <div className="col-2">{menuItem.category}</div>
+                  <div className="col-1">{menuItem.price}</div>
+                  <div className="col-2">{menuItem.specialTag}</div>
+                  <div className="col-1">
+                    <button className="btn btn-success">
+                      <i className="bi bi-pencil-fill"></i>
+                    </button>
+                    <button className="btn btn-danger mx-2">
+                      <i className="bi bi-trash-fill"></i>
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
-    </div>
+    )}
+      
+    </>
   );
 }
 
