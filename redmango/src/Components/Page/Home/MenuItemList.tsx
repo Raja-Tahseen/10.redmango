0d931549fd49708c1849407e3fc135203f59e740
Raja Tahseen <rajatahseen86@outlook.com>
2025-02-14 18:17:07 +0500

    321 - Search in Banner

--- redmango/src/Components/Page/Home/MenuItemList.tsx
+++ redmango/src/Components/Page/Home/MenuItemList.tsx
@@ -2,14 +2,38 @@ import { useState, useEffect } from "react";
 import { menuItemModel } from "../../../Interfaces";
 import MenuItemCard from "./MenuItemCard";
 import { useGetMenuItemsQuery } from "../../../Apis/menuItemApi";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setMenuItem } from "../../../Storage/Redux/menuItemSlice";
 import { MainLoader } from "../Common";
+import { RootState } from "../../../Storage/Redux/store";
 
 function MenuItemList() {
-  //const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
   const dispatch = useDispatch();
   const { data, isLoading } = useGetMenuItemsQuery(null); // Calls useGetMenuItemsQuery() to fetch the menu items. The data object contains the fetched results, and isLoading indicates the loading state.
+
+  const searchValue = useSelector(
+    (state:RootState) => state.menuItemStore.search
+  );
+
+  useEffect(() => {
+    if (data && data.result) {
+      //handleFilters();
+    }
+  },[searchValue]);
+
+  const handleFilters = (search: string) => {
+    let tempMenuItems = [...data.result];
+
+    //search FUnctionality
+    if (search) {
+      const tempSearchMenuItems = [...tempMenuItems];
+      tempMenuItems = tempSearchMenuItems.filter((item: menuItemModel) => item.name.toUpperCase().includes(search.toUpperCase()));
+    }
+    return tempMenuItems;
+  };
+
+  
 
   useEffect(() => {
     if (!isLoading) {
