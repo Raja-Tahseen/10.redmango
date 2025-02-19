f39165edb659fd6b6f61ca657cd9529456f0ec82
Raja Tahseen <rajatahseen86@outlook.com>
2025-02-19 15:11:39 +0500

    326 - Authentication and Authorization Endpoints( Add - prepareHeaders in RTK Query params)

--- redmango/src/Apis/menuItemApi.ts
+++ redmango/src/Apis/menuItemApi.ts
@@ -16,7 +16,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const menuItemApi = createApi({
     reducerPath: "menuItemApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://localhost:7181/api/"
+        baseUrl: "https://localhost:7181/api/",
+        prepareHeaders: (headers: Headers, api) => {
+            const token = localStorage.getItem("token");
+            token && headers.append("Authorization", "Bearer " + token);
+          },
     }),
     tagTypes: ["MenuItems"],
     endpoints: (builder) => ({
